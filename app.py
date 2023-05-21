2e3a031e0f213b6da224176d5505cf1b605c100f
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-05-21 05:52:37 +0300

    Changed Port

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def saveAudio():
         return jsonify({'error': str(e)})
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)  
+    app.run(host="0.0.0.0", port=8080, debug=True)  
 
 
 
