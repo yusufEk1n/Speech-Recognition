9ae07dcb9c6ecd7c191f778f33fc6ffe3331af4e
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-05-23 05:02:58 +0300

    Added Comment for audio-recorder.js

--- templates/static/js/audio-recorder.js
+++ templates/static/js/audio-recorder.js
@@ -4,85 +4,115 @@ var gumStream;
 var input;
 
 var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioContext
+var audioContext;
 
 var recordButton = document.querySelector("#recordButton");
 var stopButton = document.querySelector("#stopButton");
-var inputName = document.querySelector("#inputName")
-var recordStatus = document.querySelector("#recordStatus")
+var inputName = document.querySelector("#inputName");
+var recordStatus = document.querySelector("#recordStatus");
 
 var intervalId;
 var timeoutId;
+var myName;
 
 recordButton.addEventListener("click", startRecording);
 stopButton.addEventListener("click", stopRecording);
 
+/*
+    * Bu event listener ile inputName alanı boş ise butonu pasif değilse aktif hale getiriyoruz.
+*/
 inputName.addEventListener("input", function () {
-    if (inputName.value == "") {
-        recordButton.disabled = true;
-    } else {
-        recordButton.disabled = false;
-    }
+    recordButton.disabled = inputName.value == "";
 })
 
-function startRecording() {
-
-    var name = inputName.value
-    inputName.value = ""
+/*
+    * Bu fonksyion kayıt işlemini başlatır.
+    * Kayıt edilen ses dosyası server'a gönderilir.
+    * Her kayıt sürüsü 10 saniyedir.
+*/
+function startRecording() 
+{
+    myName = inputName.value;
+    inputName.value = "";
     inputName.disabled = true;
 
-    recordButton.disabled = true;
-    stopButton.disabled = false;
-
+    changeEnable();
 
     navigator.mediaDevices.getUserMedia({ audio: true, video: false })
         .then(function (stream) {
-
+            
+            // ses kaydı için gerekli değişkenler tanımlanıyor.
             audioContext = new AudioContext();
             gumStream = stream;
             input = audioContext.createMediaStreamSource(stream);
-            recordStatus.innerHTML = "Kayıt Başlamak Üzere..."
+            recordStatus.innerHTML = "Kayıt Başlamak Üzere...";
             
-            
+            // 10 saniyede bir tekrar eden fonksiyon ile kayıt işlemi gerçekleştiriliyor.
             intervalId = setInterval(function () {
-                recordStatus.innerHTML = "Kayıt Ediliyor..."
-                const recorder = new Recorder(input, { numChannels: 1 })
-                recorder.record()
+                recordStatus.innerHTML = "Kayıt Ediliyor...";
+                const recorder = new Recorder(input, { numChannels: 1 });
+                recorder.record();
 
+                // 10 saniye sonra kayıt işlemi durduruluyor ve postToServer fonksiyonu ile server'a gönderiliyor.
                 sleep(10000).then(() => {
-                    recorder.stop()
-                    recorder.exportWAV(function (blob) {
-                        var filename = name + "-" + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + ".wav";
-                        var formData = new FormData();
-                        formData.append("audio_data", blob, filename);
-                        console.log(filename)
-                        
-                        var request = new XMLHttpRequest();
-                        request.open("POST", "/saveAudio", true);
-                        request.send(formData);
-                        recordStatus.innerHTML = "Kayıt Edildi"
-                    });
+                    recorder.stop();
+                    recorder.exportWAV(postToServer);
                 })
             }, 11000)
 
         }).catch(function (err) {
             recordButton.disabled = false;
             stopButton.disabled = true;
-            console.log(err)
+            console.log(err);
         });
 }
 
+// Bu fonksiyon ile belirtilen süre kadar beklenir.
 const sleep = time => new Promise(resolve => timeoutId = setTimeout(resolve, time));
 
-function stopRecording() {
-
+/*
+ * Bu fonksiyon kayıt işlemini durdurur.
+ * Kayıt işlemi için kullanılan tüm değişkenler sıfırlanır.
+*/
+function stopRecording() 
+{
     recordButton.disabled = true;
     stopButton.disabled = true;
 
-    recordStatus.innerHTML = "Kayıt İşlemi Durduruldu"
+    recordStatus.innerHTML = "Kayıt İşlemi Durduruldu";
     inputName.disabled = false;
 
     gumStream.getAudioTracks()[0].stop();
-    clearInterval(intervalId)
-    clearTimeout(timeoutId)
+    clearInterval(intervalId);
+    clearTimeout(timeoutId);
+}
+
+/*
+    * Bu fonksiyon ile kayıt ve durdur butonlarının aktiflik durumu değiştirilir.
+*/
+function changeEnable()
+{
+    recordButton.disabled = !recordButton.disabled;
+    stopButton.disabled = !stopButton.disabled;
+}
+
+/*
+    * Bu fonksiyon ile rastgele bir isim ile kayıt edilen ses dosyası server'a gönderilir.
+*/
+function postToServer(blob)
+{
+    // kayıt edilen ses dosyası için girilen isim ile başlayan unique bir isim oluşturuluyor.
+    var filename = myName + "-" + Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15) + ".wav";
+
+    // Server'a gönderilecek form oluşturuluyor.
+    var formData = new FormData();
+
+    // form'a kayıt edilen ses dosyası ekleniyor.
+    formData.append("audio_data", blob, filename);
+
+    // Server'a form gönderiliyor.
+    var request = new XMLHttpRequest();
+    request.open("POST", "/saveAudio", true);
+    request.send(formData);
+    recordStatus.innerHTML = "Kayıt Edildi";
 }
